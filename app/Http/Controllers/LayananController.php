08aad7f499de66f6be625ae22bb431e9bcca13ac
Nathanael Ferry Pratama <ntnferry1505@gmail.com>
2024-07-15 15:47:17 +0700

    feat: portofolio google drive

--- app/Http/Controllers/LayananController.php
+++ app/Http/Controllers/LayananController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\JokiCategory;
+use App\Models\JokiPortfolio;
 use Illuminate\Http\Request;
 
 class LayananController extends Controller
@@ -23,7 +24,10 @@ class LayananController extends Controller
             ->with(['prices', 'portfolios'])
             ->firstOrFail();
 
+        // Retrieve the last 3 portfolios related to the category
+        $recentPortfolios = $jokiCategory->portfolios()->latest()->take(3)->get();
+
         // Pass the data to the view
-        return view('layanan.custom', compact('categories', 'jokiCategory'));
+        return view('layanan.custom', compact('categories', 'jokiCategory', 'recentPortfolios'));
     }
 }
